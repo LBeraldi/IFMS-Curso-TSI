4aaac7c782599141ba4f7ced3f2e46ed9828a6c2
Lucas Beraldi <lucas.beraldiso@gmail.com>
2024-04-29 20:01:10 -0400

    Fim de dia - Aula 29-04-2024

--- 4º Semestre/Limguagem de programação III/JavaBD/JavaBD/src/main/java/com/mycompany/javabd/FilmesDAO.java
+++ 4º Semestre/Limguagem de programação III/JavaBD/JavaBD/src/main/java/com/mycompany/javabd/FilmesDAO.java
@@ -5,6 +5,7 @@
 package com.mycompany.javabd;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,33 +18,28 @@ public class FilmesDAO {
         bd = new BD();
         bd.getConnection();
     }
-    
+
     public void incluir(Filme filme) {
         try {
-            String sql = "insert into filmes "
-                    + "(codigo, titulo, genero, produtora, datacompra) "
-                    + "values(?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO FILMES " + "(CODIGO, TITULO, GENERO, PRODUTORA, DATACOMPRA) " + "VALUES(?, ?, ?, ?, ?)";
             PreparedStatement st = bd.connection.prepareStatement(sql);
             st.setString(1, filme.getCodigo());
             st.setString(2, filme.getTitulo());
             st.setString(3, filme.getGenero());
             st.setString(4, filme.getProdutora());
             st.setString(5, filme.getDataCompra());
-            if(!st.execute()) 
-                JOptionPane.showMessageDialog(null,
-                        "Filme Cadastrado com Sucesso.");
+            if (!st.execute()) JOptionPane.showMessageDialog(null, "Filme Cadastrado com Sucesso.");
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, 
-                    "Falha ao executar SQL: " + ex.toString());
+            JOptionPane.showMessageDialog(null, "Falha ao executar SQL: " + ex.toString());
         }
         bd.close();
     }
-    
-    public void remover() {
+
+    public void remover(Filme filme) {
         try {
-            String sql = "delete from filmes where codigo = ?";
+            String sql = "DELETE FROM FILMES WHERE CODIGO = ?";
             PreparedStatement st = bd.connection.prepareStatement(sql);
-            st.setString(1, codigo);
+            st.setString(1, filme.getCodigo());
             int rowsAffected = st.executeUpdate();
             if (rowsAffected > 0) {
                 JOptionPane.showMessageDialog(null, "Filme removido com sucesso.");
@@ -54,12 +50,48 @@ public class FilmesDAO {
             JOptionPane.showMessageDialog(null, "Falha ao executar SQL: " + ex.toString());
         }
     }
-    
-    public void atualizar() {
-        
+
+    public void atualizar(Filme filme) {
+        try {
+            String sql = "UPDATE FILMES SET TITULO = ?, GENERO = ?, PRODUTORA = ?, DATACOMPRA = ? WHERE CODIGO = ?";
+            PreparedStatement st = bd.connection.prepareStatement(sql);
+            st.setString(1, filme.getTitulo());
+            st.setString(2, filme.getGenero());
+            st.setString(3, filme.getProdutora());
+            st.setString(4, filme.getDataCompra());
+            st.setString(5, filme.getCodigo());
+
+            int rowsAffected = st.executeUpdate();
+            if (rowsAffected > 0) {
+                JOptionPane.showMessageDialog(null, "Filme atualizado com sucesso.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Filme não encontrado para atualização.");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Falha ao executar SQL: " + ex.toString());
+        }
     }
-    
+
     public void consultar() {
-        
+        try {
+            String sql = "SELECT * FROM FILMES WHERE CODIGO = ?";
+            PreparedStatement st = bd.connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+
+            while (rs.next()) {
+                Filme filme = new Filme();
+                filme.setCodigo(rs.getString("codigo"));
+                filme.setTitulo(rs.getString("titulo"));
+                filme.setGenero(rs.getString("genero"));
+                filme.setProdutora(rs.getString("produtora"));
+                filme.setDataCompra(rs.getString("datacompra"));
+
+                System.out.println("Título: " + filme.getTitulo());
+                System.out.println("Gênero: " + filme.getGenero());
+                System.out.println("Produtora: " + filme.getProdutora());
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Falha ao executar SQL: " + ex.toString());
+        }
     }
 }
--- 4º Semestre/Limguagem de programação III/JavaBD/JavaBD/src/main/java/com/mycompany/javabd/Teste.java
+++ 4º Semestre/Limguagem de programação III/JavaBD/JavaBD/src/main/java/com/mycompany/javabd/Teste.java
@@ -1,2 +1,25 @@
-package com.mycompany.javabd;public class Teste {
+package com.mycompany.javabd;
+
+public class Teste {
+    public static void main(String[] args) {
+        int contador = 0;
+        int pilha = 0;
+
+        String diamante = "<..<..<>";
+        String dr = diamante.replaceAll("\\.", "");
+        System.out.println(dr);
+        char[] teste = dr.toCharArray();
+
+        for (int i = 0; i < teste.length; i++){
+            System.out.println(teste[i]);
+
+            if (teste[i] == '<'){
+                pilha++;
+            } else if (pilha > 0 && teste[i] == '>') {
+                pilha--;
+                contador++;
+            }
+        }
+        System.out.println(contador);
+    }
 }
